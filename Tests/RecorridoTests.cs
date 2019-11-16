5ab44afcf763cecd137c22299e2da7bdf31ea322
Mariano Jorreto <mariano_11887@hotmail.com>
2019-11-16 14:52:07 -0300

    Unit tests. Actualización del archivo de ayuda en línea.

---
+++ Tests/RecorridoTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class RecorridoTests
+    {
+        [TestMethod]
+        public void ObtenerTodosLosRecorridos()
+        {
+            List<Recorrido> recorridos = Recorrido.ListarTodos();
+            Assert.IsTrue(recorridos.Count > 0);
+        }
+    }
+}
